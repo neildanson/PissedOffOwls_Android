97718c379436077aaba7a3f76a664f95268a7321
neild <unknown-user@fe80::200a:f928:123b:abe5%12>
2013-06-03 08:45:56 +0100

    fixed(?) activity to not crash

--- MainActivity.fs
+++ MainActivity.fs
@@ -8,16 +8,17 @@ open Android.OS
 open Android.Runtime
 open Android.Views
 open Android.Widget
+open Microsoft.Xna.Framework
 
 [<Activity (Label = "PissedOffOwls_Android", MainLauncher = true)>]
 type MainActivity () =
-    inherit Activity ()
+    inherit AndroidGameActivity ()
 
   
     override this.OnCreate (bundle) =
 
         base.OnCreate (bundle)
-
+        PissedOffOwl.Activity <- this
         let game = new PissedOffOwl(100.0f, 100.0f)
         game.Run()
 
